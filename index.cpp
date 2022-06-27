6ebead3b5443514c5a238efe6652940d6fee345a
Davi Belo <davibelocontato@gmail.com>
2022-06-27 15:14:09 -0300

    first commit

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pessoa,ano;
+
+    cout<< "Type the old "<< endl;
+    cin >> pessoa;
+    ano = 2022-pessoa;
+    cout<< "The year that the user was born is = "<< ano << endl;
+    
+
+    return 0;
+}   
